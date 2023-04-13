fd7f792458feea8f6e57e37f445bac6ac3c35ff4
nakkoya <chanmanlok0808@gmail.com>
2023-04-13 22:00:30 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./components/Home/Home.js";
+import Navbar from "./components/Navbar/Navbar.js";
+import Auth from "./components/Auth/Auth.js";
+import UserPage from "./components/UserPage/UserPage.js";
+import Error from "./components/Error/Error.js";
+import EditPage from "./components/EditPage/EditPage.js";
+import FirstSetAvatar from "./components/Auth/FirstSetAvatar.js";
+import NotFound from "./components/Error/NotFound.js";
+import EditProfile from "./components/UserPage/EditProfile.js";
+import Padder from "./Padder.js";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Navbar />
+			<Padder />
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/home' element={<Navigate to='/' replace={true} />} />
+				<Route path='/error' element={<Error />} />
+				<Route path='/auth' element={<Auth />} />
+				<Route path='/auth/initavatar' element={<FirstSetAvatar />} />
+				<Route path='/userpage' element={<UserPage />} />
+				<Route path='/edit/expense' element={<EditPage />} />
+				<Route path='/edit/profile' element={<EditProfile />} />
+				<Route path='/*' element={<NotFound />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
